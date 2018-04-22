0950e724c6b6aec0aba4fee1deccf0f12e8ef944
Ilya Buziuk <ibuziuk@redhat.com>
2018-04-22 17:10:02 +0200

    Initial commit
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

---
+++ src/main/java/ibuziuk/spring/boot/demo/App.java
@@ -0,0 +1,13 @@
+package ibuziuk.spring.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/main/java/ibuziuk/spring/boot/demo/HelloController.java
@@ -0,0 +1,13 @@
+package ibuziuk.spring.boot.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hello World!";
+    }
+}
